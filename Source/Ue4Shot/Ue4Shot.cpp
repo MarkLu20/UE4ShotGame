e7089f4233c5e9d4f8cb0040b2cea91ed6eedb14
DESKTOP-HTLO5P7\lcc <1145230944@qq.com>
2017-07-22 16:36:10 +0800

    Initial commit

---
+++ Source/Ue4Shot/Ue4Shot.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Ue4Shot.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ue4Shot, "Ue4Shot" );
